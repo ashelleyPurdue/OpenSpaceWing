4fe6e1efe76ce78f7e7e3f07e91fbc936a9b76ae
Alex Shelley <ashelley@purdue.edu>
2016-05-02 22:57:33 -0400

    The crosshair now goes to where the bullet will actually hit, instead of 	where the player is aiming.

--- Assets/Scripts/Player/PlayerLaserSightBehaviour.cs
+++ Assets/Scripts/Player/PlayerLaserSightBehaviour.cs
@@ -33,9 +33,37 @@ public class PlayerLaserSightBehaviour : MonoBehaviour
         line.SetPosition(0, transform.position);
         line.SetPosition(1, gun.AimPoint);
 
-        //Update the crosshair pos and direction
-        crosshair.forward = -gun.AimPointNormal;
-        crosshair.position = gun.AimPoint + gun.AimPointNormal * FORWARD_OFFSET;
+        //Find the point where the bullet would *actually* hit when fired.
+        Vector3 bulletDir = (gun.AimPoint - gun.transform.position).normalized;
+        RaycastHit[] hits = Physics.RaycastAll(gun.transform.position, bulletDir, Vector3.Distance(gun.AimPoint, gun.transform.position));
+
+        bool foundHit = false;
+        RaycastHit closestHit = new RaycastHit();
+        float closestDist = float.MaxValue;
+
+        foreach (RaycastHit h in hits)
+        {
+            if (h.distance < closestDist)
+            {
+                closestDist = h.distance;
+                closestHit = h;
+
+                foundHit = true;
+            }
+        }
+
+        if (foundHit)
+        {
+            //Move the crosshair to where the bullet would hit, if a point was found.
+            crosshair.forward = closestHit.normal * -1;
+            crosshair.position = closestHit.point + closestHit.normal * FORWARD_OFFSET;
+        }
+        else
+        {
+            //If no point was found(IE: player aiming off into the distance), use the aim point
+            crosshair.forward = gun.AimPointNormal * -1;
+            crosshair.position = gun.AimPoint + gun.AimPointNormal * FORWARD_OFFSET;
+        }
     }
 
     void OnApplicationFocus(bool status)
