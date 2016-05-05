2ccd7260aea40753415ce4b278e8f1d81994b6e9
Alex Shelley <ashelley@purdue.edu>
2016-05-05 19:24:42 -0400

    Created an infinite asteroid spawner

---
+++ Assets/Scripts/InfiniteAsteroidSpawnerBehaviour.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfiniteAsteroidSpawnerBehaviour : MonoBehaviour
+{
+    public Transform player;
+
+    public float spawnDistance = 100;   //How far away from the player the asteroids should spawn
+
+    public float minX = -20;
+    public float maxX = 20;
+
+    public float minY = -20;
+    public float maxY = 20;
+
+    public float minScale = 1;
+    public float maxScale = 2;
+
+    public float minInterval = 5;
+    public float maxInterval = 20;
+
+
+    //Private fields
+    private float nextAsteroidZ;
+
+
+    //Events
+
+    void Awake()
+    {
+        nextAsteroidZ = player.position.z + Random.RandomRange(minInterval, maxInterval);
+    }
+
+    void FixedUpdate()
+    {
+        //Create an asteroid when the player crosses the current threshold
+        if (player.position.z >= nextAsteroidZ)
+        {
+            //Advance the next Z
+            nextAsteroidZ += Random.RandomRange(minInterval, maxInterval);
+
+            //Select a random position to put the asteroid
+            Vector3 newPos = Vector3.zero;
+            newPos.x = Random.RandomRange(minX, maxX);
+            newPos.y = Random.RandomRange(minY, maxY);
+            newPos.z = player.position.z + spawnDistance;
+
+            //Create the asteroid
+            GameObject asteroid = Instantiate<GameObject>(Resources.Load<GameObject>("asteroid_prefab"));
+            asteroid.transform.position = newPos;
+            asteroid.transform.localScale = Vector3.one * Random.RandomRange(minScale, maxScale);
+        }
+    }
+}
