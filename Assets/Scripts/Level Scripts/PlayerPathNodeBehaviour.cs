c2fbab10f29c1733e4caeb91fce321d0c1162628
Alex Shelley <ashelley@purdue.edu>
2016-05-09 17:41:03 -0400

    Created player path nodes, which are used to create the path that 	the player will be following.

---
+++ Assets/Scripts/Level Scripts/PlayerPathNodeBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPathNodeBehaviour : MonoBehaviour
+{
+    public PlayerPathNodeBehaviour nextNode = null;
+    public bool useDefaultSpeed = true;
+    public float speed;
+
+
+    //Events
+
+    void OnDrawGizmos()
+    {
+        //Draw a line to the next node
+        Gizmos.color = Color.white;
+        if (nextNode != null)
+        {
+            Gizmos.DrawLine(transform.position, nextNode.transform.position);
+        }
+
+        //Draw position
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, 0.1f);
+    }
+}
